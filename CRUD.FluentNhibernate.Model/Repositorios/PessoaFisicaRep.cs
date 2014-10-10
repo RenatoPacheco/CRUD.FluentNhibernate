49c8cb32404a289bf9017c8108e27ce29b201aff
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-10-10 17:51:02 -0300

    Montada a classe de conexão e os repositórios.

---
+++ CRUD.FluentNhibernate.Model/Repositorios/PessoaFisicaRep.cs
@@ -0,0 +1,14 @@
+﻿using CRUD.FluentNhibernate.CSharp.Model.Entidades;
+using CRUD.FluentNhibernate.CSharp.Model.Infras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRUD.FluentNhibernate.CSharp.Model.Repositorios
+{
+    public class PessoaFisicaRep : Repository<PessoaFisica>
+    {
+
+    }
+}

94c6c5fea37396d694cb564a4947221893df3ba5
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-10-10 18:01:09 -0300

    Adicionados os testes CRUD para pessoa física.

---
+++ CRUD.FluentNhibernate.Model.Test/_CriarEstruturaTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CRUD.FluentNhibernate.Model.Infras;
+
+namespace CRUD.FluentNhibernate.Model.Test
+{
+    [TestClass]
+    public class _CriarEstruturaTest
+    {
+
+        [TestMethod]
+        public void Cria_as_tabelas_limpando_o_banco()
+        {
+            Connection.CreatTable();
+        }
+
+        [TestMethod]
+        public void Tenta_altualizar_a_estrutura_das_tabelas()
+        {
+            Connection.UpdateTable();
+        }
+    }
+}

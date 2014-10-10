9311f1f339caacc6b0cd997319ebf5cf0e00e122
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-10-10 15:34:48 -0300

    Mapeando as entidades.

---
+++ CRUD.FluentNhibernate.Model/Maps/PessoaMap.cs
@@ -0,0 +1,43 @@
+﻿using CRUD.FluentNhibernate.CSharp.Model.Entidades;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRUD.FluentNhibernate.CSharp.Model.Maps
+{
+    public class PessoaMap : ClassMap<Pessoa>
+    {
+        public PessoaMap()
+        {
+            Table("tbl_Pessoa");
+
+            Id(x => x.Id)
+                .GeneratedBy
+                .Identity();
+
+            Map(x => x.Nome)
+               .Not.Nullable()
+               .Length(255);
+
+            Map(x => x.Login)
+               .Not.Nullable()
+               .Length(255);
+
+            Map(x => x.Senha)
+               .Not.Nullable()
+               .Length(255);
+            
+            Map(x => x.DataCriacao)
+               .Not.Nullable();
+
+            Map(x => x.DataAlteracao)
+               .Not.Nullable();
+
+            Map(x => x.Status)
+               .Not.Nullable()
+               .CustomType<int>();
+        }
+    }
+}

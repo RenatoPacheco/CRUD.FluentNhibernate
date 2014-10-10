9521423fb2ff88d63ff5773abf4a970d01cb4739
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-10-10 15:32:13 -0300

    Adicionadas interfaces para integração com o banco algumas entidades.

---
+++ CRUD.FluentNhibernate.Model/Entidades/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRUD.FluentNhibernate.CSharp.Model.Entidades
+{
+    public abstract class Pessoa
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Nome { get; set; }
+
+        public virtual string Login { get; set; }
+
+        public virtual string Senha { get; set; }
+
+        public virtual DateTime DataCriacao { get; set; }
+
+        public virtual DateTime DataAlteracao { get; set; }
+
+        public virtual Status Status { get; set; }
+    }
+}
---
+++ CRUD.FluentNhibernate.Model/Entidades/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRUD.FluentNhibernate.CSharp.Model.Entidades
+{
+    public enum Status
+    {
+        Inativo = 0,
+        Ativo = 1,
+        Bloqueado = 2,
+        Excluido = 3
+    }
+}

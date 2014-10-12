892b7998e01a53637c6ff24c2e6c2359f445e369
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-10-12 01:17:44 -0300

    Teste para a agenda.

--- CRUD.FluentNhibernate.Model.Test/AgendaTest.cs
+++ CRUD.FluentNhibernate.Model.Test/AgendaTest.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CRUD.FluentNhibernate.Model.Repositorios;
+using CRUD.FluentNhibernate.Model.Entidades;
+using System.Linq;
+using System.Diagnostics;
+using CRUD.FluentNhibernate.Model.ValueObject;
 
 namespace CRUD.FluentNhibernate.Model.Test
 {
@@ -23,8 +27,42 @@ namespace CRUD.FluentNhibernate.Model.Test
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void Registrar_uma_agenda()
         {
+            PessoaFisica pessoa = Repositorio.Query().First();
+
+            pessoa.Agendas.Add(new Agenda() { 
+                Titulo = "Um título qualquer...",
+                Local = "Não definido...",
+                Descricao = "Este evento ocorre en janeiro todos os anos.",
+                Inicio = "1/1/{0}",
+                Termino = "31/1/{0}",
+                DataCriacao = DateTime.Now,
+                DataAlteracao = DateTime.Now,
+                Status = Status.Ativo
+            });
+
+
+            Repositorio.Edit(pessoa);
+
+            DateTime referencia;
+
+            referencia = DateTime.Parse("25/1/2014");
+            Debug.WriteLine(string.Format("Início: {0} - Fim {1}",
+                pessoa.Agendas[0].Inicio.ToDateTime(referencia),
+                pessoa.Agendas[0].Termino.ToDateTime(referencia)));
+
+
+            referencia = DateTime.Parse("25/1/2015");
+            Debug.WriteLine(string.Format("Início: {0} - Fim {1}",
+                pessoa.Agendas[0].Inicio.ToDateTime(referencia),
+                pessoa.Agendas[0].Termino.ToDateTime(referencia)));
+
+
+            referencia = DateTime.Parse("25/1/2016");
+            Debug.WriteLine(string.Format("Início: {0} - Fim {1}",
+                pessoa.Agendas[0].Inicio.ToDateTime(referencia),
+                pessoa.Agendas[0].Termino.ToDateTime(referencia)));
         }
     }
 }

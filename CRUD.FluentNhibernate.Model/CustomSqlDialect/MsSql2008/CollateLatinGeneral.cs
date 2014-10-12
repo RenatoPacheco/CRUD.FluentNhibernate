182b56d6573c522dff4ceed85efdceb6c6eeaf7a
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-10-12 10:33:10 -0300

    Essa classe vai permitir que seja feita consulta ignorando caractéres latinos.

---
+++ CRUD.FluentNhibernate.Model/CustomSqlDialect/MsSql2008/CollateLatinGeneral.cs
@@ -0,0 +1,20 @@
+﻿using NHibernate;
+using NHibernate.Dialect;
+using NHibernate.Dialect.Function;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRUD.FluentNhibernate.Model.CustomSqlDialect.MsSql2008
+{
+    class CollateLatinGeneral : MsSql2008Dialect
+    {
+        public CollateLatinGeneral()
+        {
+            RegisterFunction("CollateLatinGeneral",
+              new SQLFunctionTemplate(NHibernateUtil.String,
+                  "?1 collate Latin1_general_CI_AI"));
+        }
+    }
+}
